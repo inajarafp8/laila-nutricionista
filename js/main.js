0fafec1b82eddc95d60524ab1b309408ff7aa035
inajarafp8 <inajarafp8@gmail.com>
2019-04-11 14:49:26 -0300

    Todoo os arquivos

---
+++ js/main.js
@@ -0,0 +1,122 @@
+/*Menu Hambuguer*/
+		$(document).ready(function(){
+			$('.menu').click(function(){
+				$('.header_menu ul').toggleClass('active')
+			})
+		});
+		
+
+/*Slides*/
+		$(".rslides").responsiveSlides({
+  		auto: true,             // Boolean: Animate automatically, true or false
+  		speed: 500,            // Integer: Speed of the transition, in milliseconds
+  		timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+
+		});
+
+		/*visualização da animaçãoa mais lenta*/
+
+	Visibility.onVisible(function(){
+		setTimeout(function(){
+		$(".introducao a img").addClass("animated fadeInDown");
+	},400);
+
+	setTimeout(function(){
+		$(".introducao h1").addClass("animated fadeInDown");
+	},800);	
+
+	setTimeout(function(){
+		$(".introducao p").addClass("animated fadeInDown");
+	},1200);	
+
+
+	setTimeout(function(){
+		$(".animar").addClass("animated fadeInDown");
+		},1600);
+
+	setTimeout(function(){
+		$(".animar-interno").addClass("animated fadeInDown");
+		},1200);
+
+	setTimeout(function(){
+		$(".introducao-interna h1").addClass("animated fadeInDown");
+		},400);
+
+	});
+
+	// Formulario
+
+$('.formphp').on('submit', function() {
+	var emailContato = "lailanutri13@gmail.com"; // Escreva aqui o seu e-mail
+
+	var that = $(this),
+			url = that.attr('action'),
+			type = that.attr('method'),
+			data = {};
+	
+	that.find('[name]').each(function(index, value) {
+		var that = $(this),
+				name = that.attr('name'),
+				value = that.val();
+				
+		data[name] = value;
+	});
+	
+	$.ajax({
+		url: url,
+		type: type,
+		data: data,
+		success: function(response) {
+		
+			if( $('[name="leaveblank"]').val().length != 0 ) {
+				$('.formphp').html("<div id='form-erro'></div>");
+				$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-erro');
+				});
+			} else {
+			
+				$('.formphp').html("<div id='form-send'></div>");
+				$('#form-send').html("<span>Mensagem enviada!</span><p>Em breve eu entro em contato com você. Abraços.</p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-send');
+				});
+			};
+		},
+		error: function(response) {
+			$('.formphp').html("<div id='form-erro'></div>");
+			$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+			.hide()
+			.fadeIn(1500, function() {
+			$('#form-erro');  
+		});
+		}
+	});
+	
+	return false;
+});
+
+/*Scroll do site*/
+
+$(window).scroll(function(){
+	var windowTop= $(this).scrollTop();
+
+	$('.anime').each(function(){
+		if(windowTop > $(this).offset().top - 400){
+			$(this).addClass('anime-init');
+		}else{
+			$(this).removeClass('anime-init');
+		}
+	});
+
+});
+
+
+
+
+
+
+
+
